f860b6ef9acb463a33bb2334f8e9c62e67afc1b6
Lenni009 <guentherhanslphdyt@gmail.com>
2025-02-20 11:06:30 +0100

    remove if statement

--- src/helpers/paginate.ts
+++ src/helpers/paginate.ts
@@ -1,9 +1,8 @@
 export function paginate<T>(array: T[], pageSize: number): T[][] {
   return array.reduce((acc: T[][], curr, index) => {
     const pageIndex = Math.floor(index / pageSize);
-    if (!acc[pageIndex]) {
-      acc[pageIndex] = [];
-    }
+    acc[pageIndex] ??= [];
+
     acc[pageIndex].push(curr);
     return acc;
   }, []);
