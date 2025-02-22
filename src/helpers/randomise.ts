e86aec9eedbca1a9f228c1e46914150ceba0236c
Lenni009 <guentherhanslphdyt@gmail.com>
2025-02-22 22:45:52 +0100

    use while loop

--- src/helpers/randomise.ts
+++ src/helpers/randomise.ts
@@ -5,11 +5,11 @@ export function randomiseArray<T>(arr: T[]): T[] {
   // cloning the array so we don't mutate the original array
   const orgArray = structuredClone(arr);
   const newArray: T[] = [];
-  do {
+  while (orgArray.length) {
     const randomIndex = getRndInteger(0, orgArray.length);
     const randomElement = orgArray.splice(randomIndex, 1);
     newArray.push(...randomElement);
-  } while (orgArray.length);
+  }
 
   return newArray;
 }
