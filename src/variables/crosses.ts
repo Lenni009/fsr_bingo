7411385202f8c241437223ef169152e45e346d1a
Lenni009 <guentherhanslphdyt@gmail.com>
2025-02-23 00:14:58 +0100

    remove duplicate cross

--- src/variables/crosses.ts
+++ src/variables/crosses.ts
@@ -1,6 +1,5 @@
-import x0 from '@/assets/x0.png';
 import x1 from '@/assets/x1.png';
 import x2 from '@/assets/x2.png';
 import x3 from '@/assets/x3.png';
 
-export const crosses = [x0, x1, x2, x3];
+export const crosses = [x1, x2, x3];
